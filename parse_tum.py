9312085283d276b9b7841112f4a80a7e47f2d7ed
Akshay Gulabrao <aksgula22@gmail.com>
2025-05-20 09:52:58 -0700

    improved script for parsing tum file from gt

--- parse_tum.py
+++ parse_tum.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import os
+from pathlib import Path
 
-def convert_to_tum(input_file, output_file):
+def convert_to_tum(input_file):
     """
     Convert the file to proper TUM format, handling the # comment header.
     Input format: #sec,nsec,x,y,z,qx,qy,qz,qw
     Output format: timestamp(sec.nsec) x y z qx qy qz qw
     """
+    filename = os.path.basename(input_file)[:-3] + "tum"
     df = pd.read_csv(input_file, comment='#', header=None, 
                     names=['sec', 'nsec', 'x', 'y', 'z', 'qx', 'qy', 'qz', 'qw'])
     df['timestamp'] = df['sec'] + df['nsec'] * 1e-9
     tum_df = df[['timestamp', 'x', 'y', 'z', 'qx', 'qy', 'qz', 'qw']]
-    tum_df.to_csv(output_file, sep=' ', header=False, index=False, float_format='%.9f')
+    tum_df.to_csv(filename, sep=' ', header=False, index=False, float_format='%.9f')
 
 input_filename = '/Users/ox/workspace/newer-college-1/collection1-newer-college/ground_truth/gt-nc-quad-easy.csv'
-output_filename = 'proper_tum_format.tum'
-convert_to_tum(input_filename, output_filename)
+convert_to_tum(input_filename)
